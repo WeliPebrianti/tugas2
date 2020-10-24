136aeac9354d22dbbe439fceafb6709cc41e3de3
WeliPebrianti <71173332+WeliPebrianti@users.noreply.github.com>
2020-10-24 21:34:57 +0700

    perubahan minggu ke 3

--- resources/views/login2.blade.php
+++ resources/views/login2.blade.php
@@ -160,7 +160,7 @@ a{color:inherit;text-decoration:none}
 <body>
 <div class="login-wrap">
 	<div class="login-html">
-		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Admin Weli FRUIT FRESH</label>
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Admin Wely FRUIT FRESH</label>
 		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
 		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
 		<div class="login-form">
